101c1c1cd615c1bce71ced4b1b8f7c0a0b890926
camapcon0702 <quyle070204@gmail.com>
2024-09-28 02:10:53 +0700

    add VocabularyAdapter, listview

---
+++ app/src/main/java/com/example/elitte/vocabulary_activity.java
@@ -0,0 +1,33 @@
+package com.example.elitte;
+
+import android.os.Bundle;
+import android.widget.ListView;
+
+import androidx.activity.EdgeToEdge;
+import androidx.appcompat.app.AppCompatActivity;
+import androidx.core.graphics.Insets;
+import androidx.core.view.ViewCompat;
+import androidx.core.view.WindowInsetsCompat;
+
+import java.util.List;
+
+public class vocabulary_activity extends AppCompatActivity {
+
+    private VocabularyAdapter mAdapter;
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        EdgeToEdge.enable(this);
+        setContentView(R.layout.activity_vocabulary);
+        ViewCompat.setOnApplyWindowInsetsListener(findViewById(R.id.main), (v, insets) -> {
+            Insets systemBars = insets.getInsets(WindowInsetsCompat.Type.systemBars());
+            v.setPadding(systemBars.left, systemBars.top, systemBars.right, systemBars.bottom);
+            return insets;
+        });
+
+        ListView listView = (ListView) findViewById(R.id.vocabularyList);
+        mAdapter = new VocabularyAdapter(this);
+        listView.setAdapter(mAdapter);
+    }
+}
