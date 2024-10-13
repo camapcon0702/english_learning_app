f4e635beaf9ddbc4c1f32c0e0738d01d67556f49
locnpIT <npl2004qn@gmail.com>
2024-10-13 20:21:48 +0700

    loc Loc Commit

--- app/src/main/java/com/example/elitte/Page/RegisterPage.java
+++ app/src/main/java/com/example/elitte/Page/RegisterPage.java
@@ -1,6 +1,9 @@
 package com.example.elitte.Page;
 
+import android.content.Intent;
 import android.os.Bundle;
+import android.view.View;
+import android.widget.TextView;
 
 import androidx.activity.EdgeToEdge;
 import androidx.appcompat.app.AppCompatActivity;
@@ -12,15 +15,23 @@ import com.example.elitte.R;
 
 public class RegisterPage extends AppCompatActivity {
 
+    TextView txtDangNhap;
+
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         EdgeToEdge.enable(this);
         setContentView(R.layout.activity_register_page);
-        ViewCompat.setOnApplyWindowInsetsListener(findViewById(R.id.main), (v, insets) -> {
-            Insets systemBars = insets.getInsets(WindowInsetsCompat.Type.systemBars());
-            v.setPadding(systemBars.left, systemBars.top, systemBars.right, systemBars.bottom);
-            return insets;
+
+        txtDangNhap = findViewById(R.id.dang_nhap);
+
+        txtDangNhap.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                Intent intent = new Intent(RegisterPage.this, LoginPage.class);
+                startActivity(intent);
+            }
         });
+
     }
 }
