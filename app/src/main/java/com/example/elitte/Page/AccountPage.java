dd416c722d1ba5205711b05a7a0e4e493a6b9e28
ninh24012004 <quangninhnqn24@gamil.com>
2024-10-15 13:37:46 +0700

    AccountPage and ChangePassword

---
+++ app/src/main/java/com/example/elitte/Page/AccountPage.java
@@ -0,0 +1,48 @@
+package com.example.elitte.Page;
+
+import android.content.Intent;
+import android.os.Bundle;
+import android.view.View;
+import android.widget.Button;
+import android.widget.TextView;
+
+import androidx.activity.EdgeToEdge;
+import androidx.appcompat.app.AppCompatActivity;
+import androidx.core.graphics.Insets;
+import androidx.core.view.ViewCompat;
+import androidx.core.view.WindowInsetsCompat;
+
+import com.example.elitte.R;
+
+public class AccountPage extends AppCompatActivity {
+
+    private TextView changePassword;
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        EdgeToEdge.enable(this);
+        setContentView(R.layout.activity_account_page);
+        ViewCompat.setOnApplyWindowInsetsListener(findViewById(R.id.main), (v, insets) -> {
+            Insets systemBars = insets.getInsets(WindowInsetsCompat.Type.systemBars());
+            v.setPadding(systemBars.left, systemBars.top, systemBars.right, systemBars.bottom);
+            return insets;
+        });
+
+        addControl();
+        addEvent();
+    }
+
+    private void addEvent() {
+        changePassword.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+                Intent intent = new Intent(AccountPage.this, ChangePassword.class);
+                startActivity(intent);
+            }
+        });
+    }
+
+    private void addControl(){
+        changePassword = findViewById(R.id.change_password);
+    }
+}
